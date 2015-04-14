bcd9815d3e1ebfe37154eba3a8ddb3b2fc61e6ec
formiga82 <formiga18@yahoo.com.br>
2015-04-14 17:34:20 -0300

    updated IPlayer.

--- Cluster-MC302/src/player/IPlayer.java
+++ Cluster-MC302/src/player/IPlayer.java
@@ -1,5 +1,14 @@
 package player;
 
+import java.util.Vector;
+
+import deck.card.Card;
+
 public interface IPlayer {
+	
+	public void addCard(Card card);
+	public Vector<Card> showHand();
+	public Card showCard();
+	public int howManyCards();
 
 }
--- Cluster-MC302/src/player/Player.java
+++ Cluster-MC302/src/player/Player.java
@@ -5,26 +5,25 @@ import java.util.Vector;
 import deck.card.Card;
 
 public class Player {
+	
 	Vector<Card> hand;
+	//int coins;
 	
-	public void addCard(Card card)
-	{
-		
+	public void addCard(Card card){}
+	public Vector<Card> showHand(){
+		return hand;
 	}
+	public Card showCard(int index){
+		return hand.get(index);
+	}
+	public int howManyCards(){
+		return hand.size();
+	}
+	//public int bet(){}
+	//public int highest(){}
+	//public void call(){}
+	//public void fold(){}
+	//public Card discard(int index){}
 	
-	public Vector<Card> showHand()
-	{
-		return null;
-	}
-	
-	public Card discard()
-	{
-		return null;
-	}
-	
-	public Card discard(int index)
-	{
-		return this.discard();
-	}
 	
 }
